80559180bfae7060503e2b5b01c9246b4cf88f05
same <139947934+same-oookey@users.noreply.github.com>
2023-07-31 13:09:09 +0900

    Update app.py
    メッセージの順序入れ替え

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ user_input = st.text_input("メッセージを入力してください。", key=
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
-    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+    for message in messages[1:]:  # 直近のメッセージを上に
         speaker = "🙂"
         if message["role"]=="assistant":
             speaker="🤖"
