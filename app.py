b1524e161cae000b8de9ce05cfbed427245e4e7b
same <139947934+same-oookey@users.noreply.github.com>
2023-07-31 15:55:39 +0900

    Update app.py
    変化を探す

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ def communicate():
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
-    for message in messages[1:]:  # 直近のメッセージを上に
-        speaker = "🙂"
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
         if message["role"]=="assistant":
             speaker="🤖"
+        speaker = "🙂"
 
         st.write(speaker + ": " + message["content"])
 
