09b8854537c8dd9ad0f055bc51601d108b751b8a
same <139947934+same-oookey@users.noreply.github.com>
2024-11-08 18:33:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from openai import OpenAI
-client = OpenAI()
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
