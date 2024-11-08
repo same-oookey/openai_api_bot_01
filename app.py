7eda5ca7c961b98f1b4529049b5bb3a9a34a9a58
same <139947934+same-oookey@users.noreply.github.com>
2024-11-08 18:39:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,44 @@
 import streamlit as st
 import openai
+
+# OpenAI APIキーを設定
 openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": st.secrets.AppSettings.chatbot_setting}
-        ]
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
 
+    # ユーザーメッセージを追加
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.chat.completions.create(
-        model="gpt-4o",
+    # OpenAI ChatCompletionを呼び出す
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
         messages=messages
     )
 
-    bot_message = response["choices"][0]["message"]
-    messages.append(bot_message)
+    # ChatCompletionのレスポンスからボットメッセージを取得
+    bot_message = response.choices[0].message
+    messages.append({"role": "assistant", "content": bot_message["content"]})
 
-    st.session_state["user_input"] = ""  # 入力欄を消去
-
+    # 入力欄をクリア
+    st.session_state["user_input"] = ""
 
 # ユーザーインターフェイスの構築
-
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
     for message in messages[1:]:  # 直近のメッセージを下に
         speaker = "🙂User"
-        if message["role"]=="assistant":
-            speaker="🤖Ai"
-
+        if message["role"] == "assistant":
+            speaker = "🤖Ai"
         st.write(speaker + ": " + message["content"])
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
